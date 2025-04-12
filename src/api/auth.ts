f21018a65ced2eb15d41c9343f2f0c1e6779886d
Thigas <thiagovenceslau1997@gmail.com>
2025-04-12 14:36:22 -0300

    feat: add axios in project and add auth api

---
+++ src/api/auth.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const URL_BASE = 'werewolf-api-q3lm.onrender.com'
+
+export const registerUser = async(data:{username: string,password:string, email: string}) => {
+    try {
+        const response = await axios.post(`${URL_BASE}/api/create-user/`, data);
+        return response.data;
+      } catch (error) {
+        console.error('Erro no cadastro:', error);
+        throw error;
+      }
+}
+
+export const loginUser = async(data:{username: string,password:string})   => {
+    try {
+        const response = await axios.post(`${URL_BASE}/api/login/`, data);
+        return response.data;
+      } catch (error) {
+        console.error('Erro ao efetuar login: ', error);
+        throw error;
+      }
+}
